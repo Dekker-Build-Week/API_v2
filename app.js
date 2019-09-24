866101a15f2255c4898c3f7a398797ac9b98bd18
andrewpsuedonym <j.m.walker@live.co.uk>
2019-09-24 11:36:11 +0100

    you can use `npm run start` now

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/projects', function (req, res) {
+  res.send('Hello World!');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
