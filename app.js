f77078591f14298c79cbdbac8e2e91639c0a7d1a
andrewpsuedonym <j.m.walker@live.co.uk>
2019-09-24 12:00:59 +0100

    serve all projs on /projects

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const Mongoose = require("mongoose");
 const BodyParser = require("body-parser");
 var express = require('express');
+const jsonfile = require('jsonfile');
 
 var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.get('/projects', function (req, res) {
-  res.send('Hello World!');
+  jsonfile.readFile('all_projects.json', (err, data) => {
+      if (err) throw  err;
+      res.json(data);
+  });
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
