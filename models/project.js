87b9775650ebc1e99417956fe39d04d1793ba86c
amardeep <amardeep.dosanjh@and.digital>
2019-09-25 08:54:21 +0100

    Implemented methods for project.js for create, get, remove. Mongodb connection needs to be updated to use promise

--- models/project.js
+++ models/project.js
@@ -8,10 +8,40 @@ var projectSchema = new Schema({
    media: [String] 
 });
 
+var project = mongoose.model('Project', projectSchema);
+
 function create_project(title, description, media){
     console.log('this is a stub');
+    
+    var newProject = new project({ title: title, description: description, media: media });
+    newProject.save(function (err) {
+    if (err) return console.log('Creation failed');
+    });
 }
 
+function get_project(title, description, media){
+    project.find({title: title, description: description, media: media}, function (err, project) {
+        if (err) {
+            console.log('Error retrieving project: ', err);
+        }
+        else {
+            return project;
+        }
+    });
+}
+
+function remove_project({title, description, media}) {
+    project.deleteOne({title: title, description: description, media: media}, function (err) {
+        if (err) {
+            console.log('Error removing project', err);
+        }
+        else {
+            console.log('Removing project was successful')
+        }
+    });
+}
 
 module.exports = mongoose.model('Project', projectSchema);
 module.exports.create_project = create_project;
+module.exports.get_project = get_project;
+module.exports.remove_project = remove_project;
--- test/test_projects.js
+++ test/test_projects.js
@@ -2,19 +2,30 @@ var assert = require('chai').assert;
 var expect = require('chai').expect;
 var should = require('chai').should();
 var proj_models = require('../models/project.js');
+var Mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://test_account:TestPassword123@ds227459.mlab.com:27459/build-week-db';
+Mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = Mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 describe('Project Model', function() {
   describe('#create_project', function() {
     it('should create a project', function() {
-        var test1 = {title: 't1', description: 'd1', media: ['a']};
-        proj_models.create_project(test1.title, test1.description, test1.media);
-        var test2 = proj_models.get_project(test1);
+        var test1 = {title: "t1", description: "d1", media: ["a"]};
+        //proj_models.create_project(test1.title, test1.description, test1.media);
+        var test2 = proj_models.get_project(test1.title, test1.description, test1.media);
         assert.equal(test1, test2, 'Input data should correctly be stored in the DB');
     });
     after(function() {
         var test1 = {title: 't1', description: 'd1', media: ['a']};
         proj_models.remove_project(test1);
+        //db.close();
     });
   });
 });
-
