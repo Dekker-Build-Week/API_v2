d9569c985035524e79f38b89c753baf8eb5b4383
Andrew Ajube <andrewajube@Andrews-MacBook-Pro.local>
2019-09-25 16:58:13 +0100

    added console.log messages around to see why code is not working, and also added configuration to run tests in debug mode

--- test/test_clients.js
+++ test/test_clients.js
@@ -1,27 +1,44 @@
-var assert = require('chai').assert;
-var client_model = require('../models/client.js');
-var Mongoose = require('mongoose');
+var assert = require("chai").assert;
+var client_model = require("../models/client.js");
+var Mongoose = require("mongoose");
 
 //Set up default mongoose connection
-var mongoDB = 'mongodb://test_account:TestPassword123@ds227459.mlab.com:27459/build-week-db';
+var mongoDB =
+  "mongodb://test_account:TestPassword123@ds227459.mlab.com:27459/build-week-db";
 Mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 //Get the default connection
 var db = Mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-describe('Client Model testing', function(){
-    describe('#create_client', function(){
-        var client_1 = {name: 'c1', imagePath: '1/2/3/'};
-        var new_client = client_model.create_client(client_1.name, client_1.imagePath);
-        var saved_client = client_model.get_client(client_1.name);
-        assert.equal(new_client, saved_client, 'Input data should correctly be stored in the DB')
+describe("Client Model", function() {
+  describe("#create_client", function() {
+    it("//should create a client", async function() {
+      var client_1 = { name: "c1", imagePath: "1/2/3/" };
+      console.log('LINE 20 - client_1', client_1.name, client_1.imagePath)
+      var new_client = await client_model.create_client(
+        client_1.name,
+        client_1.imagePath
+      );
+      var saved_client = await client_model.get_client(
+        client_1.name,
+        client_1.imagePath
+      );
+      console.log('LINE 29 - new client name: ' + new_client.name);
+      console.log('LINE 30 - saved client name: ' + saved_client.name);
+      assert.equal(new_client.name, saved_client.name, "name mismatch");
+      assert.equal(
+        new_client.imagePath,
+        saved_client.imagePath,
+        "image path mismatch"
+      );
     });
-    after(function(){
-        var client_1 = {name: 'c1', imagePath: '1/2/3/'};
-        client_model.remove_client(client_1);
-        //db.close();
+    after(async function() {
+      var client_1 = { name: "c1", imagePath: "1/2/3/" };
+      await client_model.remove_client(client_1);
+      db.close();
     });
+  });
 });
